7f42605b1f0b33cf3bfd0077ea454c377fe184b7
Nightshift <nightshift@Trilokn-Infotechs-Mac-mini.local>
2026-08-18 21:42:01 +0530

    Fix CSRF trusted origins

--- taskreminder/settings.py
+++ taskreminder/settings.py
@@ -31,9 +31,18 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
 
 
+
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost"
+).split(",")
+
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "CSRF_TRUSTED_ORIGINS",
+    ""
+).split(",")
 # Application definition
 
 INSTALLED_APPS = [
@@ -130,6 +139,8 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
