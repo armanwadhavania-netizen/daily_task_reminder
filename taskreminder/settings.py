232ae27a592abbddc2ab8857f7669f902f6f738f
Nightshift <nightshift@Trilokn-Infotechs-Mac-mini.local>
2026-08-18 21:25:56 +0530

    Fix static files for Railway

--- taskreminder/settings.py
+++ taskreminder/settings.py
@@ -125,6 +125,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
